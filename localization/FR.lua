f9145719b994a8e131cebafb1d7fe550e4968f90
silvereyes <silvereyes333@gmail.com>
2021-02-14 22:09:47 -0600

    Version 3.0.0
    NEW FEATURES
    -----------------
    - Update 29 Flames of Ambition compatibility
    - This version is compatible with both live (Markarth) and PTS (Flames of Ambition)
    - Improved German translations.  Thanks Scootworks!
    - Improved Russian translation.  Thanks Friday_The13_rus!
    - New option: Show Uncollected Item Set Icons
       + LibLootSummary.List:SetShowNotCollected(true/false)
       + Displays icons to the right of item names if their set pieces are not collected.
    - LAM2 controls added for existing options
       + Combine Repeated Items / LibLootSummary.List:SetCombineDuplicates(true/false)
       + Sort Order / LibLootSummary.List:SetSorted(true/false), LibLootSummary.List:SetSortedByQuality(true/false)
       + List Delimiter / LibLootSummary.List:SetDelimiter(string),
       + Link Style / LibLootSummary.List:SetLinkStyle(default/with brackets)
    - New getter and setter functions:
       + LibLootSummary.List:GetOption(optionName)
       + LibLootSummary.List:SetOption(optionName, value)
    - Calling SetOptions(options) is no longer required if you are using GenerateLam2LootOptions()
       + Pass chat, prefix and suffix parameters in to the constructor to set them all at once,
           or call them via their respective setter functions (e.g. UseLibChatMessage(), SetPrefix(), SetSuffix())
    - LibSavedVars compatibility
       + Note, there is a slight change to the parameters needed when using LibSavedVars
       + Use :GenerateLam2LootOptions(addon.name, addon.settings, addon.defaults, 'lootSummary')
           instead of :GenerateLam2LootOptions(addon.name, addon.settings.lootSummary, addon.defaults.lootSummary)
    - Performance and memory usage improvements
    
    BREAKING CHANGES
    ----------------------
    - Referencing options directly (e.g. myList.iconStyle) is no longer supported.  You must use the getter and setter functions: GetOption('optionName'), SetOption('optionName', optionValue) or Set<OptionName>(value) (e.g. SetIconStyle(value))
    - Passing chat, prefix and suffix parameters to :SetOptions() is no longer supported.  Pass them in the constructor instead.
    
    BUGFIXES
    ----------
    - Fix bug with options not reflecting their saved vars values if no defaults for them are set

--- localization/FR.lua
+++ localization/FR.lua
@@ -1,3 +1,4 @@
+
 local strings = {
     ["SI_LLS_ITEM_SUMMARY"]                    = "Récapitulatif des objets",
     ["SI_LLS_ITEM_SUMMARY_TOOLTIP"]            = "<<1>> imprimera un récapitulatif des objets à la fin.",
@@ -9,12 +10,10 @@ local strings = {
     ["SI_LLS_MIN_LOOT_QUALITY_TOOLTIP"]        = "Filtre les butins dont la qualité est inférieure à la qualité minimale indiquée dans le récapitulatif.",
     ["SI_LLS_SHOW_ITEM_ICONS"]                 = "Affiche les icônes d'objet",
     ["SI_LLS_SHOW_ITEM_ICONS_TOOLTIP"]         = "Affiche les icônes à gauche de tous les noms d'objet figurant dans le récapitulatif.",
-    ["SI_LLS_SHOW_ITEM_ICON_SIZE"]             = "Taille de l'icône d'objet",
-    ["SI_LLS_SHOW_ITEM_ICON_SIZE_TOOLTIP"]     = "Utilisez un pourcentage pour spécifier la taille des icônes qui apparaissent à gauche des noms d'objets dans le récapitulatif.",
+    ["SI_LLS_ICON_SIZE"]                       = "Taille de l'icône",
+    ["SI_LLS_ICON_SIZE_TOOLTIP"]               = "Utilisez un pourcentage pour spécifier la taille des icônes qui apparaissent dans le récapitulatif.",
     ["SI_LLS_SHOW_LOOT_ICONS"]                 = "Affiche les icônes de butin",
     ["SI_LLS_SHOW_LOOT_ICONS_TOOLTIP"]         = "Affiche les icônes à gauche de tous les noms de butin dans le récapitulatif.",
-    ["SI_LLS_SHOW_LOOT_ICON_SIZE"]             = "Taille de l'icône de butin",
-    ["SI_LLS_SHOW_LOOT_ICON_SIZE_TOOLTIP"]     = "Utilisez un pourcentage pour spécifier la taille des icônes qui apparaissent à gauche des noms de butin dans le récapitulatif.",
     ["SI_LLS_SHOW_ITEM_TRAITS"]                = "Affiche les noms de traits d'objet",
     ["SI_LLS_SHOW_ITEM_TRAITS_TOOLTIP"]        = "Affiche les noms des traits entre parenthèses à la droite des noms d'objets qui apparaissent dans le récapitulatif.",
     ["SI_LLS_SHOW_LOOT_TRAITS"]                = "Affiche les noms de traits du butin",
@@ -23,6 +22,17 @@ local strings = {
     ["SI_LLS_HIDE_ITEM_SINGLE_QTY_TOOLTIP"]    = "Les numéros de quantité ne seront imprimés qu'à la droite des noms d'objet ayant une quantité supérieure à un.",
     ["SI_LLS_HIDE_LOOT_SINGLE_QTY"]            = "N'affiche pas les quantités pour butin singulier",
     ["SI_LLS_HIDE_LOOT_SINGLE_QTY_TOOLTIP"]    = "Les numéros de quantité ne seront imprimés qu'à la droite des noms de butin avec une quantité supérieure à un.",
+    ["SI_LLS_COMBINE_DUPLICATES"]              = "Combiner des objets répétés",
+    ["SI_LLS_COMBINE_DUPLICATES_TOOLTIP"]      = "Les liens qui apparaissent plus d'une fois dans le récapitulatif sont regroupés en un seul lien et leurs quantités sont additionnées.",
+    ["SI_LLS_SHOW_ITEM_NOT_COLLECTED"]         = "<<1>> Icônes des pièces d'ensembles non collectés",
+    ["SI_LLS_SHOW_ITEM_NOT_COLLECTED_TOOLTIP"] = "Affichez les icônes à droite des noms d'objets si leurs pièces de set ne sont pas collectées.",
+    ["SI_LLS_SHOW_LOOT_NOT_COLLECTED"]         = "<<1>> Icônes des pièces d'ensembles non collectés",
+    ["SI_LLS_SHOW_LOOT_NOT_COLLECTED_TOOLTIP"] = "Affichez les icônes à droite des noms du butin si leurs pièces de set ne sont pas collectées.",
+    ["SI_LLS_SORT_ORDER_TOOLTIP"]              = "Choisissez comment vous souhaitez que le récapitulatif soit trié.",
+    ["SI_LLS_DELIMITER"]                       = "Séparateur de liste",
+    ["SI_LLS_DELIMITER_TOOLTIP"]               = "Choisissez les caractères qui séparent les entrées du récapitulatif. \"\\n\" signifie que toutes les entrées de la liste sont affichées sur des lignes séparées.",
+    ["SI_LLS_LINK_STYLE"]                      = "Apparence du lien",
+    ["SI_LLS_LINK_STYLE_TOOLTIP"]              = "Choisissez l'apparence des liens dans le récapitulatif.",
 }
 
 for stringId, value in pairs(strings) do
--- localization/JA.lua
+++ localization/JA.lua
@@ -1,3 +1,4 @@
+
 local strings = {
     ["SI_LLS_ITEM_SUMMARY"]                    = "アイテムの要約",
     ["SI_LLS_ITEM_SUMMARY_TOOLTIP"]            = "<<1>>は、完了するとアイテムの概要を印刷します。",
@@ -9,12 +10,10 @@ local strings = {
     ["SI_LLS_MIN_LOOT_QUALITY_TOOLTIP"]        = "この値より低い品質の戦利品は、戦利品ヒストリー要約に含まれません。",
     ["SI_LLS_SHOW_ITEM_ICONS"]                 = "アイテムのアイコンを表示します",
     ["SI_LLS_SHOW_ITEM_ICONS_TOOLTIP"]         = "要約に表示されるアイテム名の左側にアイコンを表示します。",
-    ["SI_LLS_SHOW_ITEM_ICON_SIZE"]             = "アイテムアイコンサイズ",
-    ["SI_LLS_SHOW_ITEM_ICON_SIZE_TOOLTIP"]     = "アイテムアイコンのサイズを増減するパーセンテージを指定します。",
+    ["SI_LLS_ICON_SIZE"]                       = "アイコンサイズ",
+    ["SI_LLS_ICON_SIZE_TOOLTIP"]               = "アイコンのサイズを増減するパーセンテージを指定します。",
     ["SI_LLS_SHOW_LOOT_ICONS"]                 = "戦利品のアイコンを表示します",
     ["SI_LLS_SHOW_LOOT_ICONS_TOOLTIP"]         = "要約に表示される戦利品名の左側にアイコンを表示します。",
-    ["SI_LLS_SHOW_LOOT_ICON_SIZE"]             = "戦利品アイコンのサイズ",
-    ["SI_LLS_SHOW_LOOT_ICON_SIZE_TOOLTIP"]     = "戦利品アイコンのサイズを増減するパーセンテージを指定します。",
     ["SI_LLS_SHOW_ITEM_TRAITS"]                = "アイテムの特性を表示する",
     ["SI_LLS_SHOW_ITEM_TRAITS_TOOLTIP"]        = "特性名がアイテム名前の右側の括弧内に表示されます。",
     ["SI_LLS_SHOW_LOOT_TRAITS"]                = "戦利品の特性を表示する",
@@ -23,6 +22,18 @@ local strings = {
     ["SI_LLS_HIDE_ITEM_SINGLE_QTY_TOOLTIP"]    = "数量番号は、1より大きい数量を持つアイテム名の右側にのみ印刷されます。",
     ["SI_LLS_HIDE_LOOT_SINGLE_QTY"]            = "単一アイテムの数量を表示しない",
     ["SI_LLS_HIDE_LOOT_SINGLE_QTY_TOOLTIP"]    = "数量番号は、1より大きい数量を持つ戦利品名の右側にのみ印刷されます。",
+    ["SI_LLS_COMBINE_DUPLICATES"]              = "繰り返されるアイテムを組み合わせる",
+    ["SI_LLS_COMBINE_DUPLICATES_TOOLTIP"]      = "要約に複数回表示されるリンクは1つのリンクに結合され、それらの数量が合計されます。",
+    ["SI_LLS_SHOW_ITEM_NOT_COLLECTED"]         = "<<1>>未収集のセットフラグメントアイコン",
+    ["SI_LLS_SHOW_ITEM_NOT_COLLECTED_TOOLTIP"] = "セットの特定のフラグメントがまだ収集されていない場合は、アイテム名の右側にアイコンが表示されます。",
+    ["SI_LLS_SHOW_LOOT_NOT_COLLECTED"]         = "<<1>>未収集のセットフラグメントアイコン",
+    ["SI_LLS_SHOW_LOOT_NOT_COLLECTED_TOOLTIP"] = "セットの特定のフラグメントがまだ収集されていない場合は、戦利品名の右側にアイコンが表示されます。",
+    ["SI_LLS_SORT_ORDER_TOOLTIP"]              = "要約を並べ替える方法を選択します。",
+    ["SI_LLS_DELIMITER"]                       = "区切り文字",
+    ["SI_LLS_DELIMITER_TOOLTIP"]               = "要約のリスト項目を区切るテキスト文字を選択します。「\\n」は、リスト内のすべてのエントリが別々の行に表示されることを意味します。",
+    ["SI_LLS_LINK_STYLE"]                      = "リンクの外観",
+    ["SI_LLS_LINK_STYLE_TOOLTIP"]              = "要約のリンクの外観を選択します。",
+    ["SI_LLS_QUOTES"]                          = GetString(SI_SAVING_EDIT_BOX_QUOTES),
 }
 
 for stringId, value in pairs(strings) do

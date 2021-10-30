0e0161944add571022ec2cd0ac4dd3d2273d46cf
Jason K. Carter <carter.jk@gmail.com>
2021-10-30 11:46:32 -0500

    Version 3.1.3
    - Update 32 Deadlands compatibility
    - This version is compatible with both PTS (Deadlands) and live (Waking Flame)

--- LibLootSummary.lua
+++ LibLootSummary.lua
@@ -2,7 +2,7 @@
 -- Distributed under the MIT license (see LICENSE.txt)          --
 ------------------------------------------------------------------
 
-LibLootSummary = { version = "3.1.2" }
+LibLootSummary = { version = "3.1.3" }
 local lls = LibLootSummary
 
 --[[ Shortcut to creating a new LibLootSumary.List instance ]]--
